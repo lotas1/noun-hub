6002e14426ae1523c5de843a1cd41d2b0c86ddc2
offor somto <offorsomto50@gmail.com>
2025-03-20 22:07:48 +0100

    Remove Swagger documentation generation from build script as it is now embedded in the Lambda function. Simplify the build process for the Go Lambda function targeting arm64 architecture.

--- backend/lambda/auth/build.sh
+++ backend/lambda/auth/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Note: Documentation is now embedded in the Lambda function
-# No need to generate Swagger docs during build
-
 # Build the Go Lambda function for AWS Lambda (arm64 architecture)
 echo "Building Lambda function..."
 GOOS=linux GOARCH=arm64 go build -tags lambda.norpc -o bootstrap main.go
